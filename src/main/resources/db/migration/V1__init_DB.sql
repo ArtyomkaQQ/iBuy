0b91f8044a84b55954a1a0956cab5a2f464c6eb0
Artyom Aralov <artyom.aralov@gmail.com>
2020-01-19 14:29:54 +0200

    Migration to Flyway DB

---
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -0,0 +1,13 @@
+create table product (
+id bigint not null auto_increment,
+description varchar(255),
+price double precision,
+quantity integer,
+primary key (id)) engine=InnoDB;
+
+create table user (
+id bigint not null auto_increment,
+email varchar(255),
+password varchar(255),
+username varchar(255),
+primary key (id)) engine=InnoDB;
