e1ac17186daab0f7d6853e4e926e9c87c2da1729
Artyom Aralov <artyom.aralov@gmail.com>
2020-02-07 12:33:43 +0200

    DB management

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -9,5 +9,5 @@ create table user (
 id bigint not null auto_increment,
 email varchar(255),
 password varchar(255),
-username varchar(255),
+username varchar(255) UNIQUE,
 primary key (id)) engine=InnoDB;
